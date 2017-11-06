6a92064b54c8447f04e60ab0d31cc740eec9b246
Jake Robbins <jrobbinsd@gmail.com>
2017-11-06 11:51:22 -0700

    updated to include new route

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-
+var todos = require('./routes/todos');
 var app = express();
 
 // view engine setup
@@ -23,6 +23,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/todos', todos);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,6 @@ module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      database: 'todos'
-    }
+    connection: 'postgres://localhost/todos'
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  }
-
-};
+}
