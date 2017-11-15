6748a80b0fc2ef514e157950b4a9b2f17df6da98
Jake Robbins <jrobbinsd@gmail.com>
2017-11-15 11:23:25 -0700

    ready to deploy

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,14 @@
 // Update with your config settings.
-
+require('dotenv').config()
 module.exports = {
 
   development: {
     client: 'pg',
     connection: 'postgres://localhost/todos'
   },
+
+	production:{
+		client: 'pg',
+		connection: process.env.DATABASE_URL
+	}
 }
